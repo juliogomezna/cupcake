92520700401e143193ce4c9f5bcfc231e729c714
DESKTOP-RCN9F1V\ASUS <julio.gomez@bpt.com.co>
2020-12-01 21:55:49 -0500

    Server Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClarityModule } from "@clr/angular";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ClarityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
